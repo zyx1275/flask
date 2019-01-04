7125417091087671434e8674c53e591f373cec59
zyx1275 <zyx1275@gmail.com>
2019-01-04 17:33:31 +0300

    3.3 hello.py. Template render

--- hello.py
+++ hello.py
@@ -1,16 +1,17 @@
-from flask import Flask
-from flask_script import Manager
+from flask import Flask, render_template
+#from flask_script import Manager
+
 app = Flask(__name__)
-manager = Manager(app)
+#manager = Manager(app)
 
 @app.route('/')
 def index():
-    return '<h1>Hello Flask!</h1>'
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s!</h1>' % name
+    return render_template('user.html', name=name)
 
 if __name__ == '__main__':
-    manager.run()    
-    #app.run(debug=True)
+    #manager.run()    
+    app.run(debug=True)
