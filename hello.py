e4d1fd39a97f0108b6aed45b06716bed5f93d86e
zyx1275 <zyx1275@gmail.com>
2019-01-05 23:49:45 +0300

    moment.js Ne rabotaet

--- hello.py
+++ hello.py
@@ -1,14 +1,18 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+
 #from flask_script import Manager
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 #manager = Manager(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
